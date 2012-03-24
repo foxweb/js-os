daefa1381ccf6b64580b3381e1cf3ff7b2d9731a
Justine Smithies <justine@smithies.me.uk>
2012-03-24 17:54:59 +0000

    Removed as unused

--- custom/build_custom.bat
+++
@@ -1,5 +0,0 @@
-
-
-copy /B /Y ff_16k.rom+ff_16k.rom+ff_16k.rom+justine.rom custom.rom
-
-rem copy /B /Y zxevo_atm.rom d:\unrealspeccy\
--- custom/make_custom.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-
-..\..\tools\asw\bin\asw -cpu z80undoc -U -L main.a80
-..\..\tools\asw\bin\p2bin main.p justine.rom -r $-$ -k
-
-rem ..\..\..\tools\mhmt\mhmt -mlz main.rom main_pack.rom
-
-pause
